0cc8f44a6155726f729a9ec3b304e51aeb149bda
Greg Osetrov <shadow-greg@yandex.ru>
2024-02-01 21:00:07 +0300

    Добавил строку поключения

--- src/DataBase/DataAccess/DataContext.cs
+++ src/DataBase/DataAccess/DataContext.cs
@@ -3,20 +3,13 @@ using Microsoft.EntityFrameworkCore;
 
 namespace DataBase;
 
-public class DataContext : DbContext {
+public class DataContext: DbContext {
     public DbSet<NewsEntity> Partners { get; set; }
 
-    public DataContext() {
-    }
+    public DataContext() { }
 
     public DataContext(DbContextOptions<DataContext> options)
-        : base(options) {
-    }
+        : base(options) { }
 
-    protected override void OnModelCreating(ModelBuilder modelBuilder) {
-    }
-    
-    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) {
-        optionsBuilder.UseNpgsql("Server=localhost;Database=MyDatabase;Trusted_Connection=True;");
-    }
+    protected override void OnModelCreating(ModelBuilder modelBuilder) { }
 }
