3ea9dd565998a76b036b42609626ddadbac6d128
Greg_Osetrov <shadow-greg@yandex.ru>
2024-02-01 16:22:13 +0300

    начал делать базу данных

---
+++ src/DataBase/DataAccess/DataContext.cs
@@ -0,0 +1,18 @@
+﻿using DataBase.Entity;
+using Microsoft.EntityFrameworkCore;
+
+namespace DataBase;
+
+public class DataContext : DbContext {
+    public DbSet<NewsEntity> Partners { get; set; }
+
+    public DataContext() {
+    }
+
+    public DataContext(DbContextOptions<DataContext> options)
+        : base(options) {
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder) {
+    }
+}
---
+++ src/DataBase/DataAccess/Entity/NewsEntity.cs
@@ -0,0 +1,9 @@
+﻿namespace DataBase.Entity;
+
+public class NewsEntity {
+    public int Id { get; set; }
+    public string? Title { get; set; }
+    public string? UrlSlug { get; set; }
+    public string? Description { get; set; }
+    public DateTime? CreatedDate { get; set; }
+}
