b61355ef4e6309be1f87b31b95efe6fa4726fb41
Greg Osetrov <shadow-greg@yandex.ru>
2024-02-02 11:38:07 +0300

    добавил тесты

--- src/Tests/TestProject/HTMLReadTests.cs
+++ src/Tests/TestProject/HTMLReadTests.cs
@@ -1,6 +1,7 @@
 ﻿using Entity;
 using HtmlAgilityPack;
 using HTMLParse;
+using HTMLParse.Abstracts;
 using HTMLParse.SiteCollection;
 
 namespace TestProject;
@@ -8,7 +9,7 @@ namespace TestProject;
 [TestFixture]
 public class HTMLReadTests {
     [Test]
-    public void GetNews_ValidUrl_ReturnsListOfNewsItems() {
+    public void Fontanka_GetNews_ValidUrl_ReturnsListOfNewsItems() {
         // Arrange
         string url = "https://www.fontanka.ru/";
         FontankaHTMLRead fontankaHtmlRead = new FontankaHTMLRead();
@@ -29,12 +30,43 @@ public class HTMLReadTests {
     }
 
     [Test]
-    public void GetNews_InvalidUrl_IsEmpty() {
+    public void Fontanka_GetNews_InvalidUrl_IsEmpty() {
         // Arrange
         string url = "https://www.fail.ru/";
-        FontankaHTMLRead fontankaHtmlRead = new FontankaHTMLRead();
+        INewsParse htmlRead = new FontankaHTMLRead();
 
         // Act & Assert
-        Assert.IsEmpty(fontankaHtmlRead.GetNews(url));
+        Assert.IsEmpty(htmlRead.GetNews(url));
+    }
+
+    [Test]
+    public void Tass_GetNews_ValidUrl_ReturnsListOfNewsItems() {
+        // Arrange
+        string url = "https://tass.ru/";
+        INewsParse htmlRead = new TassHTMLRead();
+
+        // Act
+        var news = htmlRead.GetNews(url);
+
+        // Assert
+        Assert.IsNotNull(news);
+        Assert.IsInstanceOf(typeof(List<NewsItem>), news);
+        Assert.IsTrue(news.Count > 0);
+        foreach (var item in news) {
+            Assert.IsNotNull(item.Title);
+            Assert.IsNotNull(item.UrlSlug);
+            Assert.IsNotNull(item.Description);
+            Assert.IsNotNull(item.CreatedDate);
+        }
+    }
+
+    [Test]
+    public void Tass_GetNews_InvalidUrl_IsEmpty() {
+        // Arrange
+        string url = "https://www.fail.ru/";
+        INewsParse htmlRead = new TassHTMLRead();
+
+        // Act & Assert
+        Assert.IsEmpty(htmlRead.GetNews(url));
     }
 }
