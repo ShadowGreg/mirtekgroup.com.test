6dc2bedb12303395b758fec08606945990b9c29b
Greg Osetrov <shadow-greg@yandex.ru>
2024-02-02 14:49:07 +0300

    начал набрасывать контроллер парсинга

---
+++ src/App/WebApp/Controllers/ParseController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApp.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class ParseController: ControllerBase {
+    private CancellationToken cancellationToken = new CancellationToken();
+    private bool _stop = false;
+    
+    [HttpOptions("Start")]
+    public async Task StartAsync(string url) {
+        while (!_stop) {
+            foreach (var data in mailData) {
+                bool isEmailSent = await _mailService.SendAsync(data);
+                Console.WriteLine($"Email sent: {isEmailSent} at {DateTime.Now}");
+            }
+        }
+    }
+    
+    [HttpOptions("Stop")]
+    public async Task StopAsync() {
+        _stop = true;
+        Console.WriteLine("Stopping RabbitMQ listener...");
+        _rabbitMqListener.Dispose();
+    }
+
+
+}
